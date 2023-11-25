107f56b95419464fd00744f39066612ed6323dd5
Ezz <ezmarie97@gmail.com>
2023-11-25 10:41:44 +0200

    added initial semantic structure and components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from './components/Main';
+import Nav from './components/Nav';
+import HeroSection from './components/HeroSection';
+import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
+import About from './components/About';
 
 function App() {
   return (
-    <div>Homepage</div>
+    <>
+      <Header></Header>
+
+      <Nav></Nav>
+
+      <Main>
+        <section>
+          <HeroSection></HeroSection>
+        </section>
+        <section>
+          <Highlights></Highlights>
+        </section>
+        <section>
+          <Testimonials></Testimonials>
+        </section>
+        <section>
+          <About></About>
+        </section>
+      </Main>
+
+      <Footer>
+
+      </Footer>
+    </>
   );
 }
 
